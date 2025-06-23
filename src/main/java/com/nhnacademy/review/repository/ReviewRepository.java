da566a7c284af9f982d09de066a5542960bc9bfe
sanikani <sanikani@naver.com>
2025-06-23 19:23:40 +0900

    [chore] : 불필요 메서드 삭제

--- src/main/java/com/nhnacademy/review/repository/ReviewRepository.java
+++ src/main/java/com/nhnacademy/review/repository/ReviewRepository.java
@@ -1,10 +1,7 @@
 package com.nhnacademy.review.repository;
 
-import com.nhnacademy.review.domain.dto.ReviewStatsResponse;
 import com.nhnacademy.review.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,13 +9,4 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByIsbn(String isbn);
     Optional<Review> findByIsbnAndUserId(String isbn, Long userId);
-
-    @Query("SELECT new com.nhnacademy.review.domain.dto.ReviewStatsResponse(r.bookId, AVG(r.rating), COUNT(r)) " +
-            "FROM Review r GROUP BY r.bookId HAVING COUNT(r) >= :minReviewCount ORDER BY AVG(r.rating) DESC")
-    List<ReviewStatsResponse> findBooksByAverageRatingDescWithMinReviews(@Param("minReviewCount") long minReviewCount);
-
-    @Query("SELECT new com.nhnacademy.review.domain.dto.ReviewStatsResponse(r.bookId, AVG(r.rating), COUNT(r)) " +
-            "FROM Review r GROUP BY r.bookId ORDER BY COUNT(r) DESC")
-    List<ReviewStatsResponse> findBooksByReviewCountDesc();
-
 }
